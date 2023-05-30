949e0f72df65d5f86f93f138b37f68a6614730be
julianacoelho <julianacoelho.ps@gmail.com>
2023-05-30 15:13:33 -0300

    sobre

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Cadastro from './components/Cadastro';
 import Login from './components/Login';
 import Home from './components/Home';
 import Exposicao from './components/Exposicao';
+import Sobre from './components/Sobre';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Login/>
       <Home/>
       <Exposicao/>
+      <Sobre/>
     </>
   );
 }
